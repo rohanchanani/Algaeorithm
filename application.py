51430f14666f70aa1ed5acb303ebda1085eb0145
Rohan Chanani <rohanchanani@gmail.com>
2021-07-26 19:11:14 +0000

    Completed configuration with github and pythonanywhere

--- application.py
+++ application.py
@@ -1,10 +1,9 @@
-
 # A very simple Flask Hello World app for you to get started with...
 
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return 'Hello from Flask!'
+    return render_template("index.html")
