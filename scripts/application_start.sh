e21987d71fc3da5e315a11a06e285d77e835e774
Rohan Chanani <rohanchanani@gmail.com>
2022-03-16 17:52:21 -0400

    moved requirements installation

--- scripts/application_start.sh
+++ scripts/application_start.sh
@@ -2,4 +2,5 @@
 sudo chmod -R 777 /home/ec2-user/express-app
 
 cd /home/ec2-user/algaeorithm
+sudo pip3 install -r requirements.txt
 gunicorn wsgi:app --daemon
--- scripts/before_install.sh
+++ scripts/before_install.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
 sudo yum -y install python3
 sudo yum -y install python3-pip
-sudo pip3 install -r /home/ec2-user/algaeorithm/requirements.txt
