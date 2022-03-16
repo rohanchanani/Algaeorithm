7a67a7cccb7545b186aabd4938ed3b159b7d1606
Rohan Chanani <rohanchanani@gmail.com>
2022-03-16 17:48:11 -0400

    Updated pip version

--- scripts/before_install.sh
+++ scripts/before_install.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 sudo yum -y install python3
 sudo yum -y install python3-pip
-sudo pip install -r /home/ec2-user/algaeorithm/requirements.txt
+sudo pip3 install -r /home/ec2-user/algaeorithm/requirements.txt
