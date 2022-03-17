bf70fead7335e86467034199e0924e8c8538ea8c
Rohan Chanani <rohanchanani@gmail.com>
2022-03-16 21:00:38 -0400

    First deploy to auto scale

--- scripts/application_start.sh
+++ scripts/application_start.sh
@@ -3,4 +3,4 @@ sudo chmod -R 777 /home/ec2-user/express-app
 
 cd /home/ec2-user/algaeorithm
 sudo pip3 install -r requirements.txt
-gunicorn --bind 0.0.0.0:8000 wsgi:app --daemon
+gunicorn --bind 0.0.0.0:80 wsgi:app --daemon
