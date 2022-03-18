778f539de5ddb1e80de8235d899d9d161b45b736
Rohan Chanani <rohanchanani@gmail.com>
2022-03-17 20:53:05 -0400

    removed keep-alive

--- scripts/application_start.sh
+++ scripts/application_start.sh
@@ -3,4 +3,4 @@ sudo chmod -R 777 /home/ec2-user/express-app
 
 cd /home/ec2-user/algaeorithm
 sudo pip3 install -r requirements.txt
-gunicorn --bind 0.0.0.0:80 wsgi:app --daemon --keep-alive 2
+gunicorn --bind 0.0.0.0:80 wsgi:app --daemon
