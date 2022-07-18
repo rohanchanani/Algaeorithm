6616e2618cc237ca9d3e92ea3ffecf592eaf4e9e
Rohan Chanani <rohanchanani@gmail.com>
2022-07-18 18:01:47 -0400

    Revert "testing original deployment pipeline"
    This reverts commit 6cb62adbcd7fe2f9a66c1c1eab7a4bb6a9f7a0d1.

--- scripts/application_start.sh
+++ scripts/application_start.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
-sudo chmod -R 777 /home/ubuntu/algaeorithm
+sudo chmod -R 777 /home/ec2-user/express-app
 
-cd /home/ubuntu/algaeorithm
+cd /home/ec2-user/algaeorithm
 sudo pip3 install -r requirements.txt
-sudo service nginx restart
 gunicorn --bind 0.0.0.0:80 --daemon wsgi:app
